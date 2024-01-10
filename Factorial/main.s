7859364980ab0cc058b21f41c70921f1f917b6ef
Roman <flevn@protonmail.com>
2024-01-10 11:24:27 +0100

    Changed the program to work with 64-bit host and now using printf instead of return values

--- Factorial/main.s
+++ Factorial/main.s
@@ -6,39 +6,50 @@
         #
         .section .data
 
+format_string:
+        .asciz "%d\n"
+
         .section .text
 
         .globl _start
 
 _start:
-        pushl $5        # Push the value we want to compute the factorial of.
+        pushq $8        # Push the value we want to compute the factorial of.
         call factorial
-        addl $4, %esp   # Get rid of the parameter we pushed on the stack.
-        movl %eax, %ebx # FACTORIAL returns the result in eax, but we want it in ebx because that's our return value.
-        movl $1, %eax   # Call exit function.
-        int $0x80
+        addq $8, %rsp   # Get rid of the parameter we pushed on the stack.
+
+        # Call to printf.
+        lea format_string(%rip), %rdi
+        mov %rax, %rsi
+        xor %eax, %eax
+        call printf
+        addq $8, %rsp  # Clean up the stack.
+
+        movq $60, %rax  # sys_exit system call.
+        xorq %rdi, %rdi # Return with 0 now.
+        syscall
 
         .type factorial, @function
 factorial:
-        pushl %ebp              # We have to restore ebp to its prior state before returning, so push it now.
-        movl %esp, %ebp         # We don't want to modify the stack pointer, so we use ebp.
-        movl 8(%ebp), %eax      # Store the first argument into eax.
+        pushq %rbp              # We have to restore ebp to its prior state before returoning, so push it now.
+        movq %rsp, %rbp         # We don't want to modify the stack pointer, so we use ebp.
+        movq 16(%rbp), %rax      # Store the first argument into eax.
 
-        cmpl $1, %eax           # Base case, if the number is one, we finished.
+        cmpq $1, %rax           # Base case, if the number is one, we finished.
         je end_factorial
-        cmpl $0, %eax           # Another base case!
+        cmpq $0, %rax           # Another base case!
         je end_factorial_with_zero
-        decl %eax               # Decrease the number.
-        pushl %eax              # Push it on the stack and call factorial again.
+        decq %rax               # Decrease the number.
+        pushq %rax              # Push it on the stack and call factorial again.
         call factorial
-        movl 8(%ebp), %ebx      # eax has the return value, reload our parameter into ebx.
-        imull %ebx, %eax        # Multiply that by the result of the last call to factorial. The result is in eax.
+        movq 16(%rbp), %rbx      # eax has the return value, reload our parameter into ebx.
+        imulq %rbx, %rax        # Multiply that by the result of the last call to factorial. The result is in eax.
 
 end_factorial:
-        movl %ebp, %esp         # Restore ebp and esp.
-        popl %ebp
+        movq %rbp, %rsp         # Restore ebp and esp.
+        popq %rbp
         ret
 
 end_factorial_with_zero:
-        movl $1, %eax           # The factorial of zero is one.
+        movq $1, %rax           # The factorial of zero is one.
         jmp end_factorial
