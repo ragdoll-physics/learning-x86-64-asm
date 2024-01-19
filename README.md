79d74b577ac5dc6b46612f1f55c52b04e2f686fc
Roman <flevn@protonmail.com>
2024-01-19 18:54:21 +0100

    New program

---
+++ README.md
@@ -0,0 +1,3 @@
+# Programs in GAS (GNU Assembly)
+
+Most of the programs are written in GAS. Some of them for x86-64 systems and some others for x86.
--- Records/records.s
+++ Records/records.s
@@ -1,7 +1,22 @@
         .include "linux.inc"
         .include "records.inc"
 
+        .section .bss
+
+        .lcomm record_buffer, record_size
+
         .section .data
+
+        .equ st_read_buffer, 8
+        .equ st_read_fd, 12
+        .equ st_write_buffer, 8
+        .equ st_write_fd, 12
+        .equ st_string_start_addr, 8
+
+        .equ st_open_dat_fd, -4
+        .equ st_stdout_fd, -8
+newline:
+        .asciz "\n"
 
 record1:
         .asciz "Lain"
@@ -60,20 +75,13 @@ record3:
 file_name:
         .asciz "test.dat"
 
-        .equ st_read_buffer, 8
-        .equ st_read_fd, 12
-        .equ st_write_buffer, 8
-        .equ st_write_fd, 12
-        .equ st_open_dat_fd, -4
-
-
         .section .text
 
         /* Function start. */
         .globl _start
 _start:
         movl    %esp, %ebp
-        subl    $4, %ebp
+        subl    $4, %esp
 
         movl    $sys_open, %eax
         movl    $file_name, %ebx
@@ -101,8 +109,47 @@ _start:
         addl    $8, %esp
 
         movl    $sys_close, %eax
-        movl    st_write_fd(%ebp), %ebx
+        movl    st_open_dat_fd(%ebp), %ebx
         int     $syscall
+
+        subl    $4, %esp
+
+        /* Read records. */
+        movl    $sys_open, %eax
+        movl    $file_name, %ebx
+        movl    $0, %ecx
+        movl    $0666, %edx
+        int     $syscall
+
+        /* Assuming open doesn't fail. */
+        movl    %eax, st_open_dat_fd(%ebp)
+        movl    $stdout, st_stdout_fd(%ebp)
+
+record_read_loop:
+        pushl   st_open_dat_fd(%ebp)
+        pushl   $record_buffer
+        call    read_record
+        addl    $8, %esp
+
+        cmpl    $record_size, %eax
+        jne     finished_reading
+        pushl   $record_firstname + record_buffer
+        call    count_chars
+        addl    $4, %esp
+        movl    %eax, %edx
+        movl    st_stdout_fd(%ebp), %ebx
+        movl    $sys_write, %eax
+        movl    $record_firstname + record_buffer, %ecx
+        int     $syscall
+
+        pushl   st_stdout_fd(%ebp)
+        call    write_newline
+        addl    $4, %esp
+        jmp     record_read_loop
+
+finished_reading:
+
+        /* addl    $8, %esp */
 
         movl    $sys_exit, %eax
         movl    $0, %ebx
@@ -151,6 +198,53 @@ write_record:
         int     $syscall
 
         popl    %ebx
+        movl    %ebp, %esp
+        popl    %ebp
+        ret
+
+        /*
+        * Function count_chars. Needs one argumment: a pointer to the string.
+        *
+        * Returns the count in %eax.
+        */
+        .globl count_chars
+        .type count_chars, @function
+count_chars:
+        pushl   %ebp
+        movl    %esp, %ebp
+
+        movl    $0, %ecx
+        movl    st_string_start_addr(%ebp), %edx
+
+count_loop_begin:
+        movb    (%edx), %al
+        cmpb    $0, %al
+        je      count_loop_end
+
+        incl    %ecx
+        incl    %edx
+        jmp     count_loop_begin
+
+count_loop_end:
+        movl    %ecx, %eax
+        popl    %ebp
+        ret
+
+        /*
+        * Function write_newline.
+        */
+        .globl write_newline
+        .type write_newline, @function
+write_newline:
+        pushl   %ebp
+        movl    %esp, %ebp
+
+        movl    $sys_write, %eax
+        movl    st_write_fd(%ebp), %ebx
+        movl    $newline, %ecx
+        movl    $2, %edx
+        int     $syscall
+
         movl    %ebp, %esp
         popl    %ebp
         ret
